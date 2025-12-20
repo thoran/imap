101eeb28b69c2dc82b7048ddb92b3ab85d62c2de
thoran <code@thoran.com>
2025-12-20 20:06:58 +1100

    Committed automatically by git-importall.

--- lib/IMAPClient.rb
+++ lib/IMAPClient.rb
@@ -1,23 +1,41 @@
 #!/usr/bin/env ruby
 # IMAPClient
 
-# 20131109..12
-# 0.0.0
+# 20131111
+# 0.0.1
+
+# Usage:
+# imap_client = IMAPClient.setup(server: 'mail.thoran.com', username: 'code@thoran.com', password: 'bigsecret')
+# message_ids = imap_client.search(from: 'no_reply@example.com', subject: 'Payday Loans', seen: false)
+# urls = imap_client.urls(message_ids)
+# imap_client.bye
 
 require 'net/imap'
 
-
 class IMAPClient
+
+  class << self
+
+    def setup(config)
+      imap_client = IMAPClient.new(config)
+      imap_client.login
+      imap_client
+    end
+
+  end # class << self
 
   attr_accessor :server
   attr_accessor :username
   attr_accessor :password
+  attr_reader :imap
 
-  def setup(config = {})
+  def initialize(config = {})
+    raise unless config[:server]
     @server = config[:server]
     @username = config[:username]
     @password = config[:password]
-    imap = Net::IMAP.new(@server)
+    self.mailbox = config[:mailbox]
+    @imap = Net::IMAP.new(server)
   end
 
   def login(username = nil, password = nil)
@@ -30,7 +48,7 @@ class IMAPClient
 
   def mailbox=(mailbox)
     @mailbox = mailbox
-    imap.select(@mailbox)
+    imap.select(mailbox)
   end
 
   def search(criteria = {})
@@ -50,6 +68,11 @@ class IMAPClient
         imap.store(message_id, "+FLAGS", [:Seen])
       end
     end
+  end
+
+  def bye
+    logout
+    disconnect
   end
 
   def logout
